d52f47a795b98230930a12f2bd829a8b21274b8e
34809FY98438JF843JFOFS489FY4SOF9843S4FT <zynovamoobys@outlook.com>
2025-02-13 21:57:55 -0600

    Update ScriptViewer.lua

--- modules/ScriptViewer.lua
+++ modules/ScriptViewer.lua
@@ -1,67 +1,33 @@
---[[
-	Script Viewer App Module
-	
-	A script viewer that is basically a notepad
-]]
-
--- Common Locals
-local Main,Lib,Apps,Settings -- Main Containers
-local Explorer, Properties, ScriptViewer, Notebook -- Major Apps
-local API,RMD,env,service,plr,create,createSimple -- Main Locals
+local Main, Lib, Apps, Settings
+local Explorer, Properties, ScriptViewer, Notebook
+local API, RMD, env, service, plr, create, createSimple
 
 local function initDeps(data)
-	Main = data.Main
-	Lib = data.Lib
-	Apps = data.Apps
-	Settings = data.Settings
-
-	API = data.API
-	RMD = data.RMD
-	env = data.env
-	service = data.service
-	plr = data.plr
-	create = data.create
-	createSimple = data.createSimple
+	Main, Lib, Apps, Settings = data.Main, data.Lib, data.Apps, data.Settings
+	API, RMD, env, service, plr = data.API, data.RMD, data.env, data.service, data.plr
+	create, createSimple = data.create, data.createSimple
 end
 
 local function initAfterMain()
-	Explorer = Apps.Explorer
-	Properties = Apps.Properties
-	ScriptViewer = Apps.ScriptViewer
-	Notebook = Apps.Notebook
+	Explorer, Properties, ScriptViewer, Notebook = Apps.Explorer, Apps.Properties, Apps.ScriptViewer, Apps.Notebook
 end
 
 local function main()
 	local ScriptViewer = {}
-
-	local window,codeFrame
+	local window, codeFrame
 
 	ScriptViewer.ViewScript = function(scr)
-		local s,source = pcall(env.decompile or function() end,scr)
-		if not s or not source then
-			source = "local test = 5\n\nlocal c = test + tick()\ngame.Workspace.Board:Destroy()\nstring.match('wow\\'f',\"yes\",3.4e-5,true)\ngame. Workspace.Wow\nfunction bar() print(54) end\n string . match() string 4 .match()"
-			source = source.."\n"..[==[
-			function a.sad() end
-			function a.b:sad() end
-			function 4.why() end
-			function a b() end
-			function string.match() end
-			function string.match.why() end
-			function local() end
-			function local.thing() end
-			string  . "sad" match
-			().magnitude = 3
-			a..b
-			a..b()
-			a...b
-			a...b()
-			a....b
-			a....b()
-			string..match()
-			string....match()
-			]==]
+		local success, source = pcall(env.decompile or function() end, scr)
+		if not success or not source then
+			source = [[
+local test = 5
+local c = test + tick()
+game.Workspace.Board:Destroy()
+string.match("wow\'f", "yes", 3.4e-5, true)
+game.Workspace.Wow
+function bar() print(54) end
+			]]
 		end
-
 		codeFrame:SetText(source)
 		window:Show()
 	end
@@ -69,40 +35,50 @@ local function main()
 	ScriptViewer.Init = function()
 		window = Lib.Window.new()
 		window:SetTitle("Script Viewer")
-		window:Resize(500,400)
+		window:Resize(650, 500)
 		ScriptViewer.Window = window
 
 		codeFrame = Lib.CodeFrame.new()
-		codeFrame.Frame.Position = UDim2.new(0,0,0,20)
-		codeFrame.Frame.Size = UDim2.new(1,0,1,-20)
+		codeFrame.Frame.Position = UDim2.new(0, 0, 0, 25)
+		codeFrame.Frame.Size = UDim2.new(1, 0, 1, -50)
 		codeFrame.Frame.Parent = window.GuiElems.Content
 
-		-- TODO: REMOVE AND MAKE BETTER
-		local copy = Instance.new("TextButton",window.GuiElems.Content)
-		copy.BackgroundTransparency = 1
-		copy.Size = UDim2.new(0.5,0,0,20)
-		copy.Text = "Copy to Clipboard"
-		copy.TextColor3 = Color3.new(1,1,1)
+		local buttonFrame = Instance.new("Frame", window.GuiElems.Content)
+		buttonFrame.BackgroundTransparency = 1
+		buttonFrame.Size = UDim2.new(1, 0, 0, 25)
 
-		copy.MouseButton1Click:Connect(function()
-			local source = codeFrame:GetText()
-			setclipboard(source)
+		local function createButton(text, pos, callback)
+			local btn = Instance.new("TextButton", buttonFrame)
+			btn.BackgroundTransparency = 0.3
+			btn.Size = UDim2.new(0.5, 0, 1, 0)
+			btn.Position = UDim2.new(pos, 0, 0, 0)
+			btn.Text = text
+			btn.TextColor3 = Color3.fromRGB(255, 255, 255)
+			btn.Font = Enum.Font.GothamSemibold
+			btn.TextSize = 14
+			btn.TextStrokeTransparency = 0.8
+			btn.TextButtonMode = Enum.TextButtonMode.Button
+			btn.MouseButton1Click:Connect(callback)
+			btn.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+			btn.BorderColor3 = Color3.fromRGB(50, 50, 50)
+			btn.MouseEnter:Connect(function()
+				btn.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+			end)
+			btn.MouseLeave:Connect(function()
+				btn.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+			end)
+		end
+
+		createButton("Copy to Clipboard", 0, function()
+			setclipboard(codeFrame:GetText())
 		end)
 
-		local save = Instance.new("TextButton",window.GuiElems.Content)
-		save.BackgroundTransparency = 1
-		save.Position = UDim2.new(0.5,0,0,0)
-		save.Size = UDim2.new(0.5,0,0,20)
-		save.Text = "Save to File"
-		save.TextColor3 = Color3.new(1,1,1)
-
-		save.MouseButton1Click:Connect(function()
+		createButton("Save to File", 0.5, function()
 			local source = codeFrame:GetText()
-			local filename = "Place_"..game.PlaceId.."_Script_"..os.time()..".txt"
-
-			writefile(filename,source)
-			if movefileas then -- TODO: USE ENV
-				movefileas(filename,".txt")
+			local filename = string.format("Script_%d_%d.txt", game.PlaceId, os.time())
+			writefile(filename, source)
+			if movefileas then
+				movefileas(filename, ".txt")
 			end
 		end)
 	end
@@ -110,7 +86,6 @@ local function main()
 	return ScriptViewer
 end
 
--- TODO: Remove when open source
 if gethsfuncs then
 	_G.moduleData = {InitDeps = initDeps, InitAfterMain = initAfterMain, Main = main}
 else
